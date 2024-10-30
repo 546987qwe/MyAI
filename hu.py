a27a15605175c9fc5bd15f28e5f98b449c5cb270
546987qwe <3562045486@qq.com>
2024-10-30 09:21:52 +0800

    Update hu.py

--- hu.py
+++ hu.py
@@ -4,19 +4,19 @@ pages = {
     "首页": [
         st.Page("pages/index.py", title="首页"),
     ],
-    "初级": [
-        st.Page("pages/demo1.py", title="初级"),
-    ],
-    "进阶1": [
-        st.Page("pages/demo2.py", title="进阶"),
-    ],
-    "进阶2": [
-        st.Page("pages/demo3.py", title="加强"),
-    ],
-    "进阶3": [
+    # "初级": [
+    #     st.Page("pages/demo1.py", title="初级"),
+    # ],
+    # "进阶1": [
+    #     st.Page("pages/demo2.py", title="进阶"),
+    # ],
+    # "进阶2": [
+    #     st.Page("pages/demo3.py", title="加强"),
+    # ],
+    "全知": [
         st.Page("pages/demo4.py", title="深化"),
     ],
-    "加强1": [
+    "图片生成": [
         st.Page("pages/image.py", title="图片生成"),
     ],
 }
