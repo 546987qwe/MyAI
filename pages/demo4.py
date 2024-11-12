444df350bf81773d6095d29a55e849e28cbd469a
546987qwe <3562045486@qq.com>
2024-11-12 11:52:06 +0800

    Update demo4.py

--- pages/demo4.py
+++ pages/demo4.py
@@ -22,7 +22,7 @@ chain=LLMChain(
 )
 st.title("全知")
 st.markdown("""
-       <a  href="http://localhost:8080/"><button type="button" class="a">书籍详情展示</button></a>
+       <a  href="http://zq5xpg.natappfree.cc/#/"><button type="button" class="a">书籍详情展示</button></a>
         <style>
         .a {
             position: absolute;
