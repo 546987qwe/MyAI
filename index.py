c7deccc6ad575e923f0e7217d6a2147db7306da3
546987qwe <3562045486@qq.com>
2024-11-20 15:07:20 +0800

    Update index.py

--- index.py
+++ index.py
@@ -32,7 +32,7 @@ padding-left: 12px;
 </style>
 """, unsafe_allow_html=True)
 st.markdown("""
-       <a href="http://zcddxlyi.dongtaiyuming.net/#/root/"> <button type="button">书籍详情展示</button></a>
+       <a href="http://120.46.14.243/#/root/"> <button type="button">书籍详情展示</button></a>
         <style>
         a button{
             position: absolute;
