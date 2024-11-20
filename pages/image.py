63ae6e8c63cd3fbd0a6058fc374ebf6f25992d41
546987qwe <3562045486@qq.com>
2024-11-20 15:09:03 +0800

    Update image.py

--- pages/image.py
+++ pages/image.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("AI绘画")
 st.markdown("""
-       <a  href="http://zcddxlyi.dongtaiyuming.net/#/root/"><button type="button" class="a">书籍详情展示</button></a>
+       <a  href="http://120.46.14.243/#/root/"><button type="button" class="a">书籍详情展示</button></a>
         <style>
         .a {
             position: absolute;
